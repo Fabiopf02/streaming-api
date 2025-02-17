851d6cea9bd3d4cc3d054dc6b9746e5cde63096c
Fábio França <fabio.franca@algoritme.com.br>
2025-02-17 02:38:50 -0300

    fix(search): correct youtube video listing by ID

--- src/search/search.controller.ts
+++ src/search/search.controller.ts
@@ -3,13 +3,17 @@ import {
   Get,
   HttpException,
   HttpStatus,
+  Inject,
   Query,
 } from '@nestjs/common';
 import { YoutubeService } from 'src/youtube/youtube.service';
 
 @Controller('search')
 export class SearchController {
-  constructor(private readonly youtubeService: YoutubeService) {}
+  constructor(
+    @Inject(YoutubeService)
+    private readonly youtubeService: YoutubeService,
+  ) {}
 
   @Get()
   search(@Query('query') query: string) {
@@ -19,7 +23,6 @@ export class SearchController {
   @Get('video')
   async getVideoById(@Query('id') query: string) {
     const videoSearched = await this.youtubeService.searchByYoutubeId(query);
-    return this.youtubeService.getVideoInfo(videoSearched!.url);
     if (videoSearched) return videoSearched;
     throw new HttpException('Searched video not found', HttpStatus.NOT_FOUND);
   }
